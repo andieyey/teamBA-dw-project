800df1ad70caa9fe5907a243fae4d928c03b3951
Al jorome Gonzaga <aljoromeg@gmail.com>
2025-11-18 12:19:26 +0000

    Create staging models for employees

---
+++ models/staging/stg_employees.sql
@@ -0,0 +1,13 @@
+{{ config(materialized='view') }}
+
+
+SELECT
+CAST(EmployeeID AS STRING) AS employee_id,
+FirstName AS first_name,
+MiddleInitial AS middle_initial,
+LastName AS last_name,
+SAFE_CAST(BirthDate AS DATE) AS birth_date,
+Gender AS gender,
+CAST(CityID AS STRING) AS city_id,
+SAFE_CAST(HireDate AS DATE) AS hire_date
+FROM {{ source('raw','employees') }}
