62813b0e8f71ae3d0fc87b605d492af5e752dd2b
Agatha Floreta <agathafloreta1@gmail.com>
2025-11-19 05:37:15 +0000

    fix: update staging files to deduplicate and run dbt models

--- models/staging/stg_employees.sql
+++ models/staging/stg_employees.sql
@@ -1,13 +1,14 @@
 {{ config(materialized='view') }}
 
-
 SELECT
-CAST(EmployeeID AS STRING) AS employee_id,
-FirstName AS first_name,
-MiddleInitial AS middle_initial,
-LastName AS last_name,
-SAFE_CAST(BirthDate AS DATE) AS birth_date,
-Gender AS gender,
-CAST(CityID AS STRING) AS city_id,
-SAFE_CAST(HireDate AS DATE) AS hire_date
+  DISTINCT
+  CAST(EmployeeID AS STRING) AS employee_id,
+  TRIM(FirstName) AS first_name,
+  TRIM(MiddleInitial) AS middle_initial,
+  TRIM(LastName) AS last_name,
+  SAFE_CAST(BirthDate AS DATE) AS birth_date,
+  UPPER(TRIM(Gender)) AS gender,
+  CAST(CityID AS STRING) AS city_id,
+  SAFE_CAST(HireDate AS DATE) AS hire_date
 FROM `grocery-sales-478511`.`grocery_sales_478511_grocery_sales`.`Grocery_Sales`
+WHERE EmployeeID IS NOT NULL
