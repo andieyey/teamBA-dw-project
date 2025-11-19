4f953f93c134625602c8d339270cfde620153d16
Mark Anthony Hernandez <mark.hernandez@whitecloak.com>
2025-11-19 14:54:11 +0800

    Fix NULL date handling in seasonal_sales and sales_trend_by_region
    - Add WHERE sales_date IS NOT NULL filter to prevent NULL values
    - Add WHERE month_number IS NOT NULL filter in seasonal_sales
    - Ensures all not_null tests pass successfully

--- models/marts/sales_trend_by_region.sql
+++ models/marts/sales_trend_by_region.sql
@@ -13,5 +13,6 @@ LEFT JOIN {{ ref('stg_cities') }} ci
 ON cu.city_id = ci.city_id
 LEFT JOIN {{ ref('stg_countries') }} co
 ON ci.country_id = co.country_id
+WHERE s.sales_date IS NOT NULL
 GROUP BY 1,2,3
 ORDER BY 1,4 DESC
--- models/marts/seasonal_sales.sql
+++ models/marts/seasonal_sales.sql
@@ -7,6 +7,7 @@ DATE_TRUNC(sales_date, MONTH) AS sale_month,
 EXTRACT(MONTH FROM sales_date) AS month_number,
 SUM(total_price) AS revenue
 FROM {{ ref('stg_sales') }}
+WHERE sales_date IS NOT NULL
 GROUP BY 1,2
 )
 
@@ -17,5 +18,6 @@ AVG(revenue) AS avg_monthly_revenue,
 MAX(revenue) AS max_monthly_revenue,
 MIN(revenue) AS min_monthly_revenue
 FROM monthly_sales
+WHERE month_number IS NOT NULL
 GROUP BY month_number
 ORDER BY month_number
